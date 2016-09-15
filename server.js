9e32732db3a1b1f34f85b269990fc1794581d474
Steelcow85 <m.w.goodwin@gmail.com>
2016-09-15 11:21:18 -0400

    update
    horiz and vert videos work on android, ready to start making content!

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+var portNum = 3000;
+//creat server on portNum
+var server = app.listen(portNum);
 
+//make files in public available
 app.use(express.static('public'));
 
-console.log("Socket Server Running")
+console.log("Socket Server Running on port: " + portNum);
 
 var socket = require('socket.io');
 var io = socket(server);
 
+//on new connection to server run newCOnnection
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket){
   console.log('new connection');
 
+  //if socket recieves a message called 'mouse' from client, run mouseMsg
   socket.on('mouse', mouseMsg);
 
   function mouseMsg(data){
-    var rad = 0;
-    io.emit('newMsg', rad);
+    //emit to all clients that server has been pinged with reset request
+    io.emit('newMsg', data);
     console.log("Reset Request Recieved on Server, sent back to clients");
   }
 
